4a5171a4f5085c6663bca8ec3712a3c54a2189df
luckyillia <zajchenkoi2007@gmail.com>
2025-06-26 21:55:16 +0200

    Bug fix

--- src/types/manualTypes.ts
+++ src/types/manualTypes.ts
@@ -1,8 +1,11 @@
+export type Section = {
+  id: string;
+  title: string;
+  content: React.ReactNode;
+};
+
 export type NavItem = {
   id: string;
   title: string;
   icon: string;
 };
-
-// Удаляем тип Section, так как он больше не нужен
-// Вместо этого используем только NavItem
