a526b450a4a4788e54ca5b718bebc162c09c02a6
luckyillia <zajchenkoi2007@gmail.com>
2025-06-29 23:16:20 +0200

    Replace RPTaskSection on upper position

--- src/data/manualData.ts
+++ src/data/manualData.ts
@@ -5,12 +5,12 @@ export const navItems: NavItem[] = [
     { id: 'lectures', title: 'Лекции', icon: '📚' },
     { id: 'training', title: 'Тренировки', icon: '🏃' },
     { id: 'events', title: 'Мероприятия', icon: '🎯' },
+    { id: 'rp-task', title: 'РП задания', icon: '⁉️' },
     { id: 'exam-section', title: 'Принятие экзаменов', icon: '📝' },
     { id: 'ammunition-supplies', title: 'Правила поставки боеприпасов', icon: '🚚' },
     { id: 'parking-spaces', title: 'Парковочные места ВЧ', icon: '🅿️' },
     { id: 'interview-conscript', title: 'Собеседование (Срочная)', icon: '👤' },
     { id: 'interview-contract', title: 'Собеседование (Контракт)', icon: '👥' },
-    { id: 'rp-task', title: 'РП задания', icon: '⁉️' },
     /*{ id: 'work-procedures', title: 'Процедуры работы', icon: '⚙️' },*/
     { id: 'announcements', title: 'Шаблоны для Доски Объявлений', icon: '📢' },
     { id: 'forum-responses', title: 'Работа по форуму', icon: '💬' },
