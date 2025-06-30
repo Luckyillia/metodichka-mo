6f978e65226ec96254d1d6054309b026634dbfa6
paulgood <xrustik.pasha@gmail.com>
2025-06-30 15:47:43 +0300

    Изменено название раздела экзаменов

--- src/data/manualData.ts
+++ src/data/manualData.ts
@@ -6,7 +6,7 @@ export const navItems: NavItem[] = [
     { id: 'training', title: 'Тренировки', icon: '🏃' },
     { id: 'events', title: 'Мероприятия', icon: '🎯' },
     { id: 'rp-task', title: 'РП задания', icon: '⁉️' },
-    { id: 'exam-section', title: 'Принятие экзаменов', icon: '📝' },
+    { id: 'exam-section', title: 'Правила проведения экзаменов', icon: '📝' },
     { id: 'ammunition-supplies', title: 'Правила поставки боеприпасов', icon: '🚚' },
     { id: 'parking-spaces', title: 'Парковочные места ВЧ', icon: '🅿️' },
     { id: 'interview-conscript', title: 'Собеседование (Срочная)', icon: '👤' },
