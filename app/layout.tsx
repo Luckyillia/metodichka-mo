03687f75f71c37c78463667f67d7dd1461d1072c
luckyillia <zajchenkoi2007@gmail.com>
2025-10-06 02:01:14 +0200

    Add speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter, Playfair_Display } from "next/font/google"
 import "./styles/globals.css"
 import { Suspense } from "react"
@@ -21,6 +22,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
           <Analytics />
+            <SpeedInsights />
         </AuthProvider>
       </Suspense>
       </body>
